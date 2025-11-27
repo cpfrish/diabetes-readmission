e119f707e1ed9849d0ef1414ac9dfa191c493497
Colin <cpfrish@berkeley.edu>
2025-11-27 12:51:21 -0800

    semi-final cleanup of our work

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Diabetes Readmission Prediction - Modular Package
-Final Project for DS 207
-Authors: Colin Frishberg, Terra Jiang, Sai Sriya Mudigonda, Rahil Sharma
-"""
-
-__version__ = "1.0.0"
